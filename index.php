e3876824059f6367d2266afc5503e1dca6f63e88
Portatil_Bot <Portatil_Bot@192.168.1.35>
2017-06-20 14:14:20 +0200

    Comenzar con el Repostorio

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+  <meta charset="UTF-8">
+  <title></title>
+ </head>
+ <style>
+  body{
+      padding: 0px;
+      margin: 0px;
+  }
+ </style>
+ <body>
+     <?php
+     echo "hello";
+     ?>
+ </body>
+</html>
